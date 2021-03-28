73b803d6477e36e18ec42763c18e222d3e23a7ee
adsf <adsf@DESKTOP-AMPG3UJ>
2021-03-29 00:25:59 +0800

    添加automapper的配置

--- AbpvNext.Application/AbpvNextApplicationModule.cs
+++ AbpvNext.Application/AbpvNextApplicationModule.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using Volo.Abp.Application;
+using Volo.Abp.AutoMapper;
 using Volo.Abp.Modularity;
 
 namespace AbpvNext.Application
 {
     [DependsOn(
-        typeof(AbpDddApplicationModule)
+        typeof(AbpDddApplicationModule),
+        typeof(AbpAutoMapperModule)
         )]
     public class AbpvNextApplicationModule:AbpModule
     {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpAutoMapperOptions>(opt=> {
+                opt.AddProfile<AbpvNextAutoMapperProfile>();
+            });
+            base.ConfigureServices(context);
+        }
     }
 }

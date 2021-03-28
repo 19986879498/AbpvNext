b1471c569e05f11b3a84bf51286f074eccac023b
adsf <adsf@DESKTOP-AMPG3UJ>
2021-03-29 00:43:17 +0800

    添加filter配置

---
+++ AbpvNextFilters/AbpvNextFiltersModule.cs
@@ -0,0 +1,21 @@
+﻿using AbpvNextFilters.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Volo.Abp.Modularity;
+
+namespace AbpvNextFilters
+{
+    [DependsOn(
+        
+        )]
+    public class AbpvNextFiltersModule:AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddControllersWithViews(opt=> {
+                opt.Filters.Add<AbpvNextExceptionFilterAttribute>();
+            });
+            base.ConfigureServices(context);
+        }
+    }
+}
---
+++ AbpvNextFilters/Filters/AbpvNextExceptionFilterAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbpvNextFilters.Filters
+{
+  public  class AbpvNextExceptionFilterAttribute:ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            if (!context.ExceptionHandled)
+            {
+                Console.WriteLine("出现错误"+context.Exception.Message);
+                context.Result = new ObjectResult (new {code=500, Message="程序出现异常"+context.Exception.Message });
+                context.ExceptionHandled = true;
+            }
+            base.OnException(context);
+        }
+    }
+}

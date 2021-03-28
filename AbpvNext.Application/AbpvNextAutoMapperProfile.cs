dc64515ba6914cd363ac3ed1f8827559cb6d43ab
adsf <adsf@DESKTOP-AMPG3UJ>
2021-03-29 00:23:07 +0800

    添加AutoMapper

---
+++ AbpvNext.Application/AbpvNextAutoMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AbpvNext.Application.User.Dto;
+using AbpvNext.Domain.UserInfo;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbpvNext.Application
+{
+   public class AbpvNextAutoMapperProfile:Profile
+    {
+        public AbpvNextAutoMapperProfile()
+        {
+            CreateMap<Users, UserDto>();
+        }
+    }
+}
---
+++ AbpvNext.Application/User/Dto/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace AbpvNext.Application.User.Dto
+{
+   public class UserDto:EntityDto<int>
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public int RoleId { get; set; }
+    }
+}
